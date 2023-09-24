a0111c2ffb00e912309dab0cbff5bb9dc7e8d707
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-24 18:45:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 //your code here
-function removeArticles(str) {
-  words = str.split(' ');
-  if(words.length <= 1) return str;
-  if( words[0] == 'a' || words[0] == 'the' || words[0] == 'an' )
-    return words.splice(1).join(' ');
-  return str;
+function removeArticles(touristSpots) {
+ const articlePattern = /^(a|an|the)\s/i;
+
+  // Sort the array based on lexicographic order excluding articles
+  touristSpots.sort((a, b) => {
+    const nameA = a.replace(articlePattern, '').toLowerCase();
+    const nameB = b.replace(articlePattern, '').toLowerCase();
+    return nameA.localeCompare(nameB);
+  });
+
+  // Display the sorted tourist spots
+  console.log(touristSpots);
 }
+
