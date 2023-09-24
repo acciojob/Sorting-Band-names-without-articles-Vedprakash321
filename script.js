c5b8f69c335883e31530d35e094036917329978b
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-24 18:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your code here
 function removeArticles(str) {
-  words = str.split(" ");
+  words = str.split(' ');
   if(words.length <= 1) return str;
   if( words[0] == 'a' || words[0] == 'the' || words[0] == 'an' )
-    return words.splice(1).join(" ");
+    return words.splice(1).join(' ');
   return str;
 }
